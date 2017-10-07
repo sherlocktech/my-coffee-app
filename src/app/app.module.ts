71adf7bdfbe6886c04829df734bc82d35f7ae26b
Sean Stamm <sean.stamm5@gmail.com>
2017-10-07 15:12:01 -0400

    Make double quote single quotes for import statement paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { GeolocationService } from './geolocation.service';
 import { AppComponent } from './app.component';
 import { DataService } from './data.service';
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
-          MatToolbarModule, MatCardModule, MatSlideToggleModule } from "@angular/material";
+          MatToolbarModule, MatCardModule, MatSlideToggleModule } from '@angular/material';
 import 'hammerjs';
 import { ListComponent } from './list/list.component';
 import { CoffeeComponent } from './coffee/coffee.component';
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'coffee', component: CoffeeComponent },
