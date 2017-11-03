9eed1a4a39e4124bb3b857ce446080e98ed59602
Sean Stamm <sean.stamm5@gmail.com>
2017-11-03 10:21:32 -0400

    Add cards to coffee and styling

--- src/app/logic/Coffee.ts
+++ src/app/logic/Coffee.ts
@@ -9,6 +9,11 @@ export class Coffee {
   notes: string;
   tastingRating: TastingRating;
 
-  constructor(public name: string, public place: string, 
-    public location: PlaceLocation) { }
+  constructor(public name: string = "",
+              public place: string = "",
+              public location: PlaceLocation = null) { 
+
+    this.location = new PlaceLocation();
+    this.tastingRating = new TastingRating();
+  }
 }
