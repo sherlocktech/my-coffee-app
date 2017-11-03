5c30bb7283e45dce3f02ca7fdbf14d6a11b9196f
Sean Stamm <sean.stamm5@gmail.com>
2017-11-03 10:22:20 -0400

    Add routing and update DataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
+          MatToolbarModule, MatCardModule, MatSlideToggleModule, MatFormFieldModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+          
+import 'hammerjs';
+
 import { GeolocationService } from './geolocation.service';
 import { AppComponent } from './app.component';
 import { DataService } from './data.service';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
-          MatToolbarModule, MatCardModule, MatSlideToggleModule } from '@angular/material';
-import 'hammerjs';
 import { ListComponent } from './list/list.component';
 import { CoffeeComponent } from './coffee/coffee.component';
-import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: ListComponent },
@@ -26,6 +29,7 @@ const routes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(routes),
+    FormsModule, HttpModule,
     BrowserModule, BrowserAnimationsModule, MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
     MatToolbarModule, MatCardModule, MatSlideToggleModule
   ],
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Coffee } from './logic/Coffee';
 import { PlaceLocation } from './logic/PlaceLocation';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  public endpoint = "http://localhost:3000";
 
   getList(callback) {
-    // TODO: Change it with a real Web Service
-    const list = [
-      new Coffee("Double Espresso", "Sunny Cafe", new PlaceLocation("123 Market St", "San Francicso")),
-      new Coffee("Caramel Americano", "Starcoffee", new PlaceLocation("Gran Via 34", "Madrid"))
-    ];
-    callback(list);
+    // const list = [
+    //   new Coffee("Double Espresso", "Sunny Cafe", new PlaceLocation("123 Market St", "San Francicso")),
+    //   new Coffee("Caramel Americano", "Starcoffee", new PlaceLocation("Gran Via 34", "Madrid"))
+    // ];
+    // callback(list);
+    this.http.get(`${this.endpoint}/coffees`)
+      .subscribe(response => {
+        console.log(response.json());
+        callback(response.json());
+      });
   }
 
   save(coffee, callback) {
-    // TODO: Change it with a real Web Service
-    callback(true);
+    if (coffee._id) {
+      // It's an update
+      this.http.put(`${this.endpoint}/coffees/${coffee._id}`, coffee)
+        .subscribe(response => {
+          callback(true);
+        });
+    } else {
+      // It's an insert
+      this.http.post(`${this.endpoint}/coffees/`, coffee)
+        .subscribe(response => {
+          callback(true);
+        });
+    }
   }
 
 }
